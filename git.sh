f209f5f660828e7cb281e1af06fcd64e385edcf5
Yvonne Radsmikham <yvonne.radsmikham@gmail.com>
2019-01-15 14:27:49 -0800

    Update git.sh

--- git.sh
+++ git.sh
@@ -49,6 +49,8 @@ cd fabrikate/examples/getting-started
 fab install
 fab generate prod
 
+ls
+
 # SSH Key
 
 #echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCxmugaMunQx/qfDrHUGxI4BizksEl9YyayuBOn+FQNnr2jbz+jaOhYYU2mgMds3gWBmKNVvIpNIuUFm1o78MDKlYh5HkWhXbPiGnarhIlbKhfuX0kIjirbzbryxbVxclKcUvIWlujETCXT6v3PAltknxOLyV1mKnbdLIbMOZw/ssb/2MnOUH06v6hBo+n9/eYVMFDtj1WYNeru9rAVsa9NjZeGwCb2B8QS2X7i82OT8DOaRNvACAuPVOSa61DKinRi2IEFdGY3+5UPkWKrvUnJ0y5DgLVuxlSv9oS/cCB/IhJbHuiwpLBfP1OVjJieMKNCOdAK2MD0azKh3TFH/3suizWa7OecxgRqU844KnIBpxdzfQNTiiR8CWVkQNhHEE23BxLLBVGAuneM/Mqrt1tGRsrztGS2+LfWsorJkDBIGScwkpri7bTaCaGqTA8KuwE//LNk3RiN+c/KMkZwb2uvDi6uCg16u8n+kZnH54VKHmgFJPY2fwPhkOPpKW0PAGVo7pvidW4E00mQ1OlQIxr2jphZ1mUddYajNC6nzcSTGDuL0z04RxNXonKdpKX526jNFFrnMoV+yMOh3nQ/HQvg39v4ZZjgHMRUTF4e1I4UOb/B8Ul69DI37bG0bqS3fbrxQYRPszIZgT32Uqi/dYOvR0WxkkzLhXnPOCqc8S48mQ=="
@@ -70,7 +72,7 @@ git checkout master
 echo "GIT STATUS"
 git status
 echo "GIT ADD"
-cp fabrikate/examples/getting-started/generated/prod/infra/elasticsearch-fluentd-kibana/*.yaml /home/vsts/work/1/s/walmart-k8s
+cp /home/vsts/work/1/s/fabrikate/examples/getting-started/generated/prod/infra/elasticsearch-fluentd-kibana/*.yaml /home/vsts/work/1/s/walmart-k8s
 git add *.yml
 echo "GIT COMMIT"
 git commit -m "Updated k8s manifest files"
