88732e24cfe868d28ea998d167e42b8d2941e02c
Yvonne Radsmikham <yvonne.radsmikham@gmail.com>
2019-01-15 13:37:58 -0800

    Update git.sh

--- git.sh
+++ git.sh
@@ -11,8 +11,7 @@ chmod -R 777 ~/.ssh/
 touch ~/.ssh/known_hosts
 
 echo "listing contents in .ssh directory..."
-cd ~/.ssh/
-ls -a
+ls ~/.ssh/
 
 # Add GitHub.com to known_hosts file
 echo "Adding Github.com to known_hosts file..."
@@ -34,6 +33,9 @@ ls ~/.ssh/
 eval `ssh-agent -s`
 ssh-add
 
+# Tighten security for private key
+chmod 400 ~/.ssh/id_rsa
+
 # Fabrikate
 echo "Downloading Fabrikate..."
 wget "https://github.com/Microsoft/fabrikate/releases/download/0.1.2/fab-v0.1.2-linux-amd64.zip"
@@ -47,10 +49,13 @@ fab generate prod
 
 # SSH Key
 
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCxmugaMunQx/qfDrHUGxI4BizksEl9YyayuBOn+FQNnr2jbz+jaOhYYU2mgMds3gWBmKNVvIpNIuUFm1o78MDKlYh5HkWhXbPiGnarhIlbKhfuX0kIjirbzbryxbVxclKcUvIWlujETCXT6v3PAltknxOLyV1mKnbdLIbMOZw/ssb/2MnOUH06v6hBo+n9/eYVMFDtj1WYNeru9rAVsa9NjZeGwCb2B8QS2X7i82OT8DOaRNvACAuPVOSa61DKinRi2IEFdGY3+5UPkWKrvUnJ0y5DgLVuxlSv9oS/cCB/IhJbHuiwpLBfP1OVjJieMKNCOdAK2MD0azKh3TFH/3suizWa7OecxgRqU844KnIBpxdzfQNTiiR8CWVkQNhHEE23BxLLBVGAuneM/Mqrt1tGRsrztGS2+LfWsorJkDBIGScwkpri7bTaCaGqTA8KuwE//LNk3RiN+c/KMkZwb2uvDi6uCg16u8n+kZnH54VKHmgFJPY2fwPhkOPpKW0PAGVo7pvidW4E00mQ1OlQIxr2jphZ1mUddYajNC6nzcSTGDuL0z04RxNXonKdpKX526jNFFrnMoV+yMOh3nQ/HQvg39v4ZZjgHMRUTF4e1I4UOb/B8Ul69DI37bG0bqS3fbrxQYRPszIZgT32Uqi/dYOvR0WxkkzLhXnPOCqc8S48mQ=="
+#echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCxmugaMunQx/qfDrHUGxI4BizksEl9YyayuBOn+FQNnr2jbz+jaOhYYU2mgMds3gWBmKNVvIpNIuUFm1o78MDKlYh5HkWhXbPiGnarhIlbKhfuX0kIjirbzbryxbVxclKcUvIWlujETCXT6v3PAltknxOLyV1mKnbdLIbMOZw/ssb/2MnOUH06v6hBo+n9/eYVMFDtj1WYNeru9rAVsa9NjZeGwCb2B8QS2X7i82OT8DOaRNvACAuPVOSa61DKinRi2IEFdGY3+5UPkWKrvUnJ0y5DgLVuxlSv9oS/cCB/IhJbHuiwpLBfP1OVjJieMKNCOdAK2MD0azKh3TFH/3suizWa7OecxgRqU844KnIBpxdzfQNTiiR8CWVkQNhHEE23BxLLBVGAuneM/Mqrt1tGRsrztGS2+LfWsorJkDBIGScwkpri7bTaCaGqTA8KuwE//LNk3RiN+c/KMkZwb2uvDi6uCg16u8n+kZnH54VKHmgFJPY2fwPhkOPpKW0PAGVo7pvidW4E00mQ1OlQIxr2jphZ1mUddYajNC6nzcSTGDuL0z04RxNXonKdpKX526jNFFrnMoV+yMOh3nQ/HQvg39v4ZZjgHMRUTF4e1I4UOb/B8Ul69DI37bG0bqS3fbrxQYRPszIZgT32Uqi/dYOvR0WxkkzLhXnPOCqc8S48mQ=="
 
 # Git
 echo "Cloning Git Repo..."
+git clone git@github.com:yradsmikham/walmart-k8s
+echo "List content of repo..."
+ls -a
 echo "GIT CHECKOUT MASTER"
 git checkout master
 echo "GIT STATUS"
