57d2908fd20e5634da9910de4599ff72f0a05dce
Yvonne Radsmikham <yvonne.radsmikham@gmail.com>
2019-01-17 15:08:57 -0800

    Update build_pat.sh

--- build_pat.sh
+++ build_pat.sh
@@ -1,5 +1,5 @@
 echo "Downloading Fabrikate..."
-wget "https://github.com/Microsoft/fabrikate/releases/download/0.1.3/fab-v-linux-amd64.zip"
+wget "https://github.com/Microsoft/fabrikate/releases/download/0.1.2/fab-v0.1.2-linux-amd64.zip"
 unzip fab-v0.1.2-linux-amd64.zip -d fab
 export PATH=$PATH:/home/vsts/work/1/s/fab
 
