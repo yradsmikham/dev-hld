1c9bc138cfa599ed7ae9d558f623c394c34f954d
Yvonne Radsmikham <yvonne.radsmikham@gmail.com>
2019-01-17 15:33:51 -0800

    Update build_pat.sh

--- build_pat.sh
+++ build_pat.sh
@@ -10,13 +10,16 @@ fab generate prod
 
 
 cd /home/vsts/work/1/s/
+echo "GIT CLONE"
 git clone https://github.com/yradsmikham/walmart-k8s.git
 cd walmart-k8s
-git checkout master
 
+echo "GIT CHECKOUT"
+git checkout master
 echo "GIT STATUS"
 git status
 echo "Copy yaml files to repo directory..."
+rm -rf prod/
 cp -r /home/vsts/work/1/s/fabrikate/examples/getting-started/generated/* /home/vsts/work/1/s/walmart-k8s
 ls /home/vsts/work/1/s/walmart-k8s
 echo "GIT ADD"
