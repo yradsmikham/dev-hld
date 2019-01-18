bd684f2b8e4b776279f49f7daa6d6d6691cce5bb
Yvonne Radsmikham <yvonne.radsmikham@gmail.com>
2019-01-18 09:37:02 -0800

    updated shell script

--- cicd/build_pat.sh
+++ cicd/build_pat.sh
@@ -3,6 +3,7 @@ pwd
 echo "List content in current directory:"
 ls -a
 
+cd /home/vsts/work/1/s/
 echo "Downloading Fabrikate..."
 wget "https://github.com/Microsoft/fabrikate/releases/download/0.1.2/fab-v0.1.2-linux-amd64.zip"
 unzip fab-v0.1.2-linux-amd64.zip -d fab
