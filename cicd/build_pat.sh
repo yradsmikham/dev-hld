ac1556e017db1d5e0435bd813fb619a041a7e461
Yvonne Radsmikham <yvonne.radsmikham@gmail.com>
2019-01-29 11:49:27 -0800

    Update build_pat.sh

--- cicd/build_pat.sh
+++ cicd/build_pat.sh
@@ -1,7 +1,6 @@
 cd /home/vsts/work/1/s/
 
-# Store the ouput of `curl -s https://api.github.com/repos/Microsoft/fabrikate/tags`
-# If the release number is not provided, then download the latest
+# If the version number is not provided, then download the latest
 if [ -z "$VERSION" ]
 then
     VERSIONS=$(curl -s https://api.github.com/repos/Microsoft/fabrikate/tags)
@@ -13,23 +12,23 @@ fi
 
 echo "Downloading Fabrikate..."
 echo "Latest Fabrikate Version: $LATEST_VERSION"
-#wget "https://github.com/Microsoft/fabrikate/releases/download/0.1.2/fab-v0.1.2-linux-amd64.zip"
 wget "https://github.com/Microsoft/fabrikate/releases/download/$LATEST_VERSION/fab-v$LATEST_VERSION-linux-amd64.zip"
-#unzip fab-v0.1.2-linux-amd64.zip -d fab
 unzip fab-v$LATEST_VERSION-linux-amd64.zip -d fab
 export PATH=$PATH:/home/vsts/work/1/s/fab
 fab install
 
-#git clone https://github.com/Microsoft/fabrikate
-#cd fabrikate/examples/getting-started
 fab generate prod
 echo "FAB GENERATE PROD COMPLETED"
 ls -a
 
 cd /home/vsts/work/1/s/
 echo "GIT CLONE"
+git clone $AKS_MANIFEST_REPO_URL # Example: https://github.com/yradsmikham/walmart-k8s.git
 git clone https://github.com/yradsmikham/walmart-k8s.git
-cd walmart-k8s
+url = https://github.com/yradsmikham/walmart-k8s.git
+basename=$(basename $url)
+repo_name=${basename%.*}
+cd repo_name
 
 echo "GIT CHECKOUT"
 git checkout master
